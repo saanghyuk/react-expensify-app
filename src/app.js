a53675642c0d6cd1a27eae328ee546ca4a25caec
Sanghyuk <saanghyuk@gmail.com>
2017-10-15 18:33:02 +0900

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -4,20 +4,26 @@ import {Provider} from 'react-redux';
 import 'normalize.css/normalize.css'
 import configureStore from './store/configureStroe'
 import './styles/styles.scss';
-import AppRouter from'./routers/AppRouter'
+import AppRouter from'./routers/AppRouter';
+import 'react-dates/lib/css/_datepicker.css';
 import {addExpense} from "./actions/expenses";
 import {setTextFilter} from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
-import 'react-dates/lib/css/_datepicker.css';
+
+
+
+
 const store =configureStore();
 
-store.dispatch(addExpense({description: "Water Bill", amount:4500}));
-store.dispatch(addExpense({description: "Gas Bill", createdAt: 1000}));
-store.dispatch(addExpense({description: "Rent", amount:109500}));
+// store.dispatch(addExpense({description: "Water Bill", amount:4500}));
+// store.dispatch(addExpense({description: "Gas Bill", createdAt: 1000}));
+// store.dispatch(addExpense({description: "Rent", amount:109500}));
+//
+// const state=store.getState();
 
-const state=store.getState();
-const visibleExpenses=getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+console.log('test');
+// const visibleExpenses=getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
-//entry --> output
-const path=require('path');
+const path = require('path');
+const ExtractTextPlugin =require('extract-text-webpack-plugin');
 
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
-module.exports={
-    entry: './src/app.js', //main file 어디서 시작할지
-    output:{
-        path: path.join(__dirname, 'public'),
-        filename:'bundle.js'
-    },
-    module:{
-        rules:[{
-            loader : 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]//두개 array로 사용 가능
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer:{
-        contentBase:path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options:{
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
