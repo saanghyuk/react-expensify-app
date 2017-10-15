aba62e2bdf82a43966f76d64f8ee31abbcada43d
Sanghyuk <saanghyuk@gmail.com>
2017-10-15 22:54:35 +0900

    Setup formatting for amount and createdAt

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -3,6 +3,7 @@ import {ExpenseListItem} from '../../components/ExpenseListItem';
 import {shallow} from 'enzyme';
 import expenses from '../fixtures/expenses'
 
+
 test('should render ExpenseListItem with expense', ()=>{
     const expense=expenses[1];
     const wrapper=shallow(<ExpenseListItem key={expense.id} {...expense}/>);
