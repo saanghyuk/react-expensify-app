bf7bf67273ef28177a9ae436b6f6de60779e1383
Sanghyuk <saanghyuk@gmail.com>
2017-10-20 03:05:59 +0900

    Setup startRemoveExpense with async error

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,12 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import {
+    startAddExpense, addExpense, editExpense,
+    removeExpense, setExpenses, startSetExpenses,
+    startEditExpense, startRemoveExpense
+} from '../../actions/expenses';
+
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
@@ -12,18 +17,34 @@ beforeEach((done)=>{
     expenses.forEach(({id, description, note, amount, createdAt})=>{
         expensesData[id]={ description, note, amount, createdAt};
     });
-   database.ref('expenses').set(expensesData).then(()=>done());
+    database.ref('expenses').set(expensesData).then(()=>done());
 });
+
 
 
 test('should setup remove expense action object', () => {
-  const action = removeExpense({ id: '123abc' });
-  expect(action).toEqual({
-    type: 'REMOVE_EXPENSE',
-    id: '123abc'
-  });
+    const action = removeExpense({ id: '123abc' });
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
 });
 
+test('should remove expense from firebase', (done)=>{
+    const store = createMockStore({});
+    const id = expenses[2].id;
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toBeFalsy();
+        done();
+    });
+});
 test('should setup edit expense action object', () => {
   const action = editExpense('123abc', { note: 'New note value' });
   expect(action).toEqual({
@@ -113,4 +134,8 @@ test('should fetch the expenses from firebase', (done)=>{
        });
        done()
     });
+});
+
+test('should edit the expense from firebase', (done)=>{
+
 });
