b014e4ac46557d66926922affc5e29732c489de0
Sanghyuk <saanghyuk@gmail.com>
2017-10-20 01:26:05 +0900

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -25,7 +25,6 @@ export const startAddExpense = (expenseData = {}) => {
         });
     };
 };
-
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
@@ -38,3 +37,25 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
