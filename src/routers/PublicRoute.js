2c7b46e7a313a587676101846056328be030d037
Sanghyuk <saanghyuk@gmail.com>
2017-10-21 22:08:57 +0900

    Store user data in user area

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -5,19 +5,16 @@ import Header from '../components/Header'
 //component: Component는 그냥 rename하는 거야
 //...rest는 지금 아직 안쓴애들 다 펼쳐 주는 거야
 //인자로 props다 들어가니깐 그냥 destructuring 해 주는 것
-export const PrivateRoute = ({
+export const PublicRoute = ({
                                  isAuthenticated,
                                  component: Component,
                                  ...rest
                              }) => (
     <Route {...rest} component={(props) => ( //여기서 props는 위 component겠지
         isAuthenticated ? (
-            <div>
-               <Header />
-               <Component {...props} />
-            </div>
+            <Redirect to="/dashboard" />
         ) : (
-            <Redirect to="/" />
+            <Component {...props} />
         )
     )} />
 );
@@ -26,4 +23,6 @@ const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
+export default connect(mapStateToProps)(PublicRoute);
+
+
